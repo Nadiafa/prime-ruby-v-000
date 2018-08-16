b441eb362dd5bd4e60d6da332c065ebc7e2c0fca
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 16:05:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(n)
-  if num <= 0 || num == 1 
+  if n <= 0 || n == 1 
     false
   else
-    true
+    (2..n-1).to_a.all? do |factor|
+      n % factor != 0
+    end
   end
 end
